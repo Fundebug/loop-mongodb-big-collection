be744dd56700dd919fe46ffc3c6ef0715c400428
kiwenlau <kiwenlau@126.com>
2019-03-16 17:05:26 +0800

    遍历的时候打印member的name

--- test1.js
+++ test1.js
@@ -6,6 +6,7 @@ async function test() {
     let N = 0;
     await Promise.mapSeries(members, member => {
         N++;
+        console.log(`name of the ${N}th member: ${member.name}`);
     });
     console.log(`loop all ${N} members success`);
 }
--- test2.js
+++ test2.js
@@ -5,6 +5,7 @@ async function test() {
     let N = 0;
     await membersCursor.eachAsync(member => {
         N++;
+        console.log(`name of the ${N}th member: ${member.name}`);
     });
     console.log(`loop all ${N} members success`);
 }
